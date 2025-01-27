490a8e5545b422f35f4124a297f639ccfa785e6a
olavea <ola@olavea.com>
2025-01-27 13:53:03 +0100

    tailwindcss #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	integrations: [
 		starlight({
 			title: 'My Docs',
