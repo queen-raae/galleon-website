4fe0fde57faba15e8b01206f134054a0cbec2a11
raae <queen@raae.codes>
2025-02-18 18:28:12 +0100

    fix: use standard gray

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,20 @@
 import colors from "tailwindcss/colors";
 import starlightPlugin from "@astrojs/starlight-tailwind";
 
+// Generated color palettes
+const accent = {
+  200: colors.orange[200],
+  600: colors.orange[600],
+  900: colors.orange[900],
+  950: colors.orange[950],
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.orange,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.yellow,
-      },
+      colors: { accent },
       fontFamily: {
         // Your preferred code font. Starlight uses system monospace fonts by default.
         serif: ['"DM Serif Text"'],
