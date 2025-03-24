49351e3a8be5e66e15a273f6597687c7e49ab670
raae <queen@raae.codes>
2025-03-24 10:28:42 +0100

    feat: add outseta authenticated test endpoint

---
+++ src/pages/v1/outseta/me.ts
@@ -0,0 +1,69 @@
+import type { APIRoute } from "astro";
+import { jwtVerify, createRemoteJWKSet, decodeJwt } from "jose";
+
+export const prerender = false;
+
+const verifyToken = async (token: string | undefined) => {
+  if (!token) {
+    return null;
+  }
+
+  try {
+    const decoded = decodeJwt(token);
+
+    // Fetch the JSON Web Key (JWK) set
+    const JWKS = createRemoteJWKSet(
+      // ❕ Exchange for your outseta url
+      new URL(`${decoded.iss}/.well-known/jwks`),
+    );
+
+    // Use the JSON Web Key (JWK) to verify the token
+    const { payload } = await jwtVerify(token, JWKS);
+
+    // Respond
+    return payload;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export const GET: APIRoute = async ({ request }) => {
+  const token = request.headers.get("Authorization")?.split(" ")[1];
+
+  // Verify the token
+  const payload = await verifyToken(token);
+
+  if (!payload) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized",
+      }),
+      {
+        headers,
+        status: 401,
+      },
+    );
+  }
+
+  // Respond
+  return new Response(
+    JSON.stringify({
+      greeting: `Hello ${payload?.name || "Pirate"}`,
+      name: payload?.name,
+      email: payload?.email,
+      personUid: payload?.sub,
+      accountUid: payload?.["outseta:accountUid"],
+      planUid: payload?.["outseta:planUid"],
+      isPrimary: payload?.["outseta:isPrimary"],
+      addOnUids: payload?.["outseta:addOnUids"],
+    }),
+    {
+      headers,
+    },
+  );
+};
