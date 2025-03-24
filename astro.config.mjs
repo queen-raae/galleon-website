6c0027f3215a8f6e182a1191967c46043a217039
raae <queen@raae.codes>
2025-03-24 10:30:46 +0100

    feat: add Netlify adapter and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://galleon.tools",
   integrations: [tailwind(), mdx()],
+  adapter: netlify(),
 });
