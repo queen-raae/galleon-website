e7494e1cf7f6a6ab8ea1a25c478556cc94a103e8
raae <queen@raae.codes>
2025-03-25 12:58:17 +0100

    add OPTIONS api route

--- src/pages/v1/queen.ts
+++ src/pages/v1/queen.ts
@@ -1,4 +1,4 @@
-import type { APIRoute } from 'astro'
+import type { APIRoute } from "astro";
 
 const headers = {
   "Content-Type": "application/json",
@@ -7,24 +7,29 @@ const headers = {
 };
 
 export const GET: APIRoute = () => {
-  
   // Respond
   return new Response(
     JSON.stringify({
-        name: 'Queen Raae',
-        bio: 'Adipisicing officia commodo est id do voluptate minim Lorem sunt ullamco consectetur Lorem. Duis dolor laboris mollit. Aliquip laboris veniam pariatur aute. Cillum nisi sint amet sit aute. Velit in pariatur et. Laboris adipisicing commodo dolore ullamco in nulla nostrud aliquip velit tempor aliqua dolor nulla ullamco.',
-        avatar: {
-            url: 'https://avatars.githubusercontent.com/u/90453561?v=4',
-            alt: '',
-        },
-        socials: [
-            { url: 'http://twitter.com/raae', label: 'Twitter' },
-            { url: 'http://github.com/raae', label: 'GitHub' }
-        ]   
+      name: "Queen Raae",
+      bio: "Adipisicing officia commodo est id do voluptate minim Lorem sunt ullamco consectetur Lorem. Duis dolor laboris mollit. Aliquip laboris veniam pariatur aute. Cillum nisi sint amet sit aute. Velit in pariatur et. Laboris adipisicing commodo dolore ullamco in nulla nostrud aliquip velit tempor aliqua dolor nulla ullamco.",
+      avatar: {
+        url: "https://avatars.githubusercontent.com/u/90453561?v=4",
+        alt: "",
+      },
+      socials: [
+        { url: "http://twitter.com/raae", label: "Twitter" },
+        { url: "http://github.com/raae", label: "GitHub" },
+      ],
     }),
     {
       headers,
     },
-  )
-}
+  );
+};
 
+export const OPTIONS: APIRoute = () => {
+  return new Response(null, {
+    headers,
+    status: 204,
+  });
+};
